8142a1f35e136b8119955525cbd06242f795ffc3
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2020-10-17 23:37:16 +0100

    Add some constants, testing connection
    Signed-off-by: Diogo Jerónimo <diogo10jeronimo@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::io::BufReader;
+use std::io::BufWriter;
+
+fn main() -> std::io::Result<()> {
+    let stream = TcpStream::connect("192.168.1.187:22")?;
+
+    let mut reader = BufReader::new(&stream);
+    let mut writer = BufWriter::new(&stream);
+
+    let mut data = String::new();
+    reader.read_line(&mut data)?;
+    println!("{:?}", &data);
+
+    // SSH-2.0-Simple_Rust_Client_1.0
+    let buf: [u8; 32] = [83, 83, 72, 45, 50, 46, 48, 45, 83, 105, 109, 112, 108, 101, 95, 82, 117, 115, 116, 95, 67, 108, 105, 101, 110, 116, 95, 49, 46, 48, 13, 10];
+    writer.write(&buf)?;
+    writer.flush()?;
+
+    let mut data2 = Vec::new();
+    reader.read_until(0x00, &mut data2)?;
+    println!("{:?}", &data2);
+    Ok(())
 }
---
+++ src/numbers.rs
@@ -0,0 +1,50 @@
+struct Message;
+
+impl Message {
+    pub const SSH_MSG_DISCONNECT: u8 = 1;
+    pub const SSH_MSG_IGNORE: u8 = 2;   
+    pub const SSH_MSG_UNIMPLEMENTED: u8 = 3;    
+    pub const SSH_MSG_DEBUG: u8 = 4;
+    pub const SSH_MSG_SERVICE_REQUEST: u8 = 5; 
+    pub const SSH_MSG_SERVICE_ACCEPT: u8 = 6;
+    pub const SSH_MSG_KEXINIT: u8 = 20;
+    pub const SSH_MSG_NEWKEYS: u8 = 21;
+    pub const SSH_MSG_USERAUTH_REQUEST: u8 = 50;
+    pub const SSH_MSG_USERAUTH_FAILURE: u8 = 51; 
+    pub const SSH_MSG_USERAUTH_SUCCESS: u8 = 52;   
+    pub const SSH_MSG_GLOBAL_REQUEST: u8 = 80;    
+    pub const SSH_MSG_REQUEST_SUCCESS: u8 = 81;
+    pub const SSH_MSG_REQUEST_FAILURE: u8 = 82;   
+    pub const SSH_MSG_CHANNEL_OPEN: u8 = 90;   
+    pub const SSH_MSG_CHANNEL_OPEN_CONFIRMATION: u8 = 91;  
+    pub const SSH_MSG_CHANNEL_OPEN_FAILURE: u8 = 92; 
+    pub const SSH_MSG_CHANNEL_WINDOW_ADJUST: u8 = 93;  
+    pub const SSH_MSG_CHANNEL_DATA: u8 = 94;   
+    pub const SSH_MSG_CHANNEL_EXTENDED_DATA: u8 = 95; 
+    pub const SSH_MSG_CHANNEL_EOF: u8 = 96;
+    pub const SSH_MSG_CHANNEL_CLOSE: u8 = 97;
+    pub const SSH_MSG_CHANNEL_REQUEST: u8 = 98;
+    pub const SSH_MSG_CHANNEL_SUCCESS: u8 = 99;
+    pub const SSH_MSG_CHANNEL_FAILURE: u8 = 100;
+}
+
+struct DisconnectionMessage;
+
+impl DisconnectionMessage {
+    pub const SSH_DISCONNECT_HOST_NOT_ALLOWED_TO_CONNECT: u8 = 1;
+    pub const SSH_DISCONNECT_PROTOCOL_ERROR: u8 = 2;
+    pub const SSH_DISCONNECT_KEY_EXCHANGE_FAILED: u8 = 3;
+    pub const SSH_DISCONNECT_RESERVED: u8 = 4;
+    pub const SSH_DISCONNECT_MAC_ERROR: u8 = 5;
+    pub const SSH_DISCONNECT_COMPRESSION_ERROR: u8 = 6;
+    pub const SSH_DISCONNECT_SERVICE_NOT_AVAILABLE: u8 = 7;
+    pub const SSH_DISCONNECT_PROTOCOL_VERSION_NOT_SUPPORTED: u8 = 8;
+    pub const SSH_DISCONNECT_HOST_KEY_NOT_VERIFIABLE: u8 = 9;
+    pub const SSH_DISCONNECT_CONNECTION_LOST: u8 = 10;
+    pub const SSH_DISCONNECT_BY_APPLICATION: u8 = 11;
+    pub const SSH_DISCONNECT_TOO_MANY_CONNECTIONS: u8 = 12;
+    pub const SSH_DISCONNECT_AUTH_CANCELLED_BY_USER: u8 = 13;
+    pub const SSH_DISCONNECT_NO_MORE_AUTH_METHODS_AVAILABLE: u8 = 14;
+    pub const SSH_DISCONNECT_ILLEGAL_USER_NAME: u8 = 15;
+}
+
