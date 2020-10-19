d61f74d4effd355728a4bc406b18966a79d94a1a
Diogo Jerónimo <diogo10jeronimo@gmail.com>
2020-10-20 00:15:11 +0100

    Refactor constant files, test key exchange
    Signed-off-by: Diogo Jerónimo <diogo10jeronimo@gmail.com>

--- src/connection.rs
+++ src/connection.rs
@@ -5,6 +5,8 @@ use std::io::BufWriter;
 use std::net::IpAddr;
 use std::str;
 
+mod algorithms;
+
 pub fn read_line(reader: &mut BufReader<&TcpStream>) -> std::io::Result<String> {
     let mut data = String::new();
     reader.read_line(&mut data)?;
@@ -20,6 +22,7 @@ pub fn write_line(writer: &mut BufWriter<&TcpStream>, line: &str) -> std::io::Re
 // Testing SSH handshake
 // 1. Sending Client Identifier
 // 2. Sending Algorithm List
+// 3. Testing key exchange
 
 pub fn run(host: IpAddr, port: u16) -> std::io::Result<()>{
     let socket = SocketAddr::new(host, port);
@@ -59,6 +62,64 @@ pub fn run(host: IpAddr, port: u16) -> std::io::Result<()>{
     }
 
     println!("Server algorithms: {:?}", server_algorithms);
+
+    let mut ciphers: Vec<u8> = Vec::new();
+
+    ciphers.push(20);
+    ciphers.append(&mut vec![0x69,0x45,0x32,0x50,0xc5,0x59,0x60,0x52,0x1c,0xf9,0xd5,0xc6,0x38,0x47,0xa8,0x50]);
+    ciphers.append(&mut (algorithms::KEY_EXCHANGE_ALGORITHM.len() as u32).to_be_bytes().to_vec());
+    ciphers.append(&mut algorithms::KEY_EXCHANGE_ALGORITHM.as_bytes().to_vec());
+    ciphers.append(&mut (algorithms::PUBLIC_KEY_ALGORITHM.len() as u32).to_be_bytes().to_vec());
+    ciphers.append(&mut algorithms::PUBLIC_KEY_ALGORITHM.as_bytes().to_vec());
+    ciphers.append(&mut (algorithms::ENCRYPTION_ALGORITHM.len() as u32).to_be_bytes().to_vec());
+    ciphers.append(&mut algorithms::ENCRYPTION_ALGORITHM.as_bytes().to_vec());
+    ciphers.append(&mut (algorithms::ENCRYPTION_ALGORITHM.len() as u32).to_be_bytes().to_vec());
+    ciphers.append(&mut algorithms::ENCRYPTION_ALGORITHM.as_bytes().to_vec());
+    ciphers.append(&mut (algorithms::MAC_ALGORITHM.len() as u32).to_be_bytes().to_vec());
+    ciphers.append(&mut algorithms::MAC_ALGORITHM.as_bytes().to_vec());
+    ciphers.append(&mut (algorithms::MAC_ALGORITHM.len() as u32).to_be_bytes().to_vec());
+    ciphers.append(&mut algorithms::MAC_ALGORITHM.as_bytes().to_vec());
+    ciphers.append(&mut (algorithms::COMPRESSION_ALGORITHM.len() as u32).to_be_bytes().to_vec());
+    ciphers.append(&mut algorithms::COMPRESSION_ALGORITHM.as_bytes().to_vec());
+    ciphers.append(&mut (algorithms::COMPRESSION_ALGORITHM.len() as u32).to_be_bytes().to_vec());
+    ciphers.append(&mut algorithms::COMPRESSION_ALGORITHM.as_bytes().to_vec());
+    ciphers.append(&mut vec![0;23]);
+
+    let cipher_list_len = ((ciphers.len() + 1) as u32).to_be_bytes().to_vec();
+
+    let mut i = 0;
+    for b in cipher_list_len.iter() {
+        ciphers.insert(i, *b);
+        i += 1;
+    }
+
+    ciphers.insert(i, 0xa);
+
+    writer.write(ciphers.as_slice())?;
+    writer.flush()?;
+
+
+    let mut key_exchange: Vec<u8> = Vec::new();
+
+    key_exchange.push(30);
+    key_exchange.push(0);
+    key_exchange.push(0);
+    key_exchange.push(0);
+    key_exchange.push(0x20);
+    key_exchange.append(&mut vec![0x69,0x45,0x32,0x50,0xc5,0x59,0x60,0x52,0x1c,0xf9,0xd5,0xc6,0x38,0x47,0xa8,0x50,0x69,0x45,0x32,0x50,0xc5,0x59,0x60,0x52,0x1c,0xf9,0xd5,0xc6,0x38,0x47,0xa8,0x50]);
+    key_exchange.append(&mut vec![0;6]);
+
+    let key_exchange_len = ((key_exchange.len() + 1) as u32).to_be_bytes().to_vec();    
+
+    let mut j = 0;
+    for b in key_exchange_len.iter() {
+        key_exchange.insert(j, *b);
+        j += 1;
+    }
+
+    key_exchange.insert(j, 6);
+
+    writer.write(key_exchange.as_slice())?;
 
     Ok(())
 }
---
+++ src/connection/algorithms.rs
@@ -0,0 +1,14 @@
+// Key Exchange Method Names
+pub const KEY_EXCHANGE_ALGORITHM: &str = "curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,diffie-hellman-group14-sha256";
+
+// Public Key Algorithm Names
+pub const PUBLIC_KEY_ALGORITHM: &str = "ecdsa-sha2-nistp256";
+
+// Encryption Algorithm Names
+pub const ENCRYPTION_ALGORITHM: &str = "chacha20-poly1305@openssh.com,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com";
+
+// MAC Algorithm Names
+pub const MAC_ALGORITHM: &str = "umac-64-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha1-etm@openssh.com,umac-64@openssh.com,umac-128@openssh.com,hmac-sha2-256,hmac-sha2-512,hmac-sha1";
+
+// Compression Algorithm Names
+pub const COMPRESSION_ALGORITHM: &str = "none,zlib@openssh.com";
